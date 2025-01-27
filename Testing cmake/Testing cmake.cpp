ce632f6051b1b949c4d89d35aa97378811e5ce41
MathsPOP <87388486+MathsPOP@users.noreply.github.com>
2025-01-27 14:48:59 +0000

    Add files via upload

---
+++ Testing cmake/Testing cmake.cpp
@@ -0,0 +1,12 @@
+﻿// Testing cmake.cpp : Defines the entry point for the application.
+//
+
+#include "Testing cmake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
